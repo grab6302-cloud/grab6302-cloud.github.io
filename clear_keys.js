e34d80382d92a185d7c184147d5d45a10b49886b
QuizHelper <user@quiz.helper>
2026-03-16 16:11:10 +0700

    UI tweak: auto-hide tool panels after 3 seconds on error

---
+++ clear_keys.js
@@ -0,0 +1,7 @@
+const fs=require('fs');
+fetch('https://api.jsonbin.io/v3/b/69b42732b7ec241ddc66a506/latest', {headers:{'X-Master-Key':'$2a$10$oYANSzZQ3hm27sTDDedds.DbkKQZObqPZieEJ/bg8i/hnh/2SMs8u'}})
+.then(r=>r.json()).then(j=>{
+   var d=j.record||j;
+   d._keys={};
+   return fetch('https://api.jsonbin.io/v3/b/69b42732b7ec241ddc66a506',{method:'PUT',headers:{'Content-Type':'application/json','X-Master-Key':'$2a$10$oYANSzZQ3hm27sTDDedds.DbkKQZObqPZieEJ/bg8i/hnh/2SMs8u'},body:JSON.stringify(d)});
+}).then(()=>console.log("Keys Cleared!"));
